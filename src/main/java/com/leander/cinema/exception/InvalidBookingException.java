3e20d05d07984c48fed9d079488b1180b3222a35
Ellinor Leander <el222nj@student.lnu.se>
2025-10-16 21:22:07 +0200

    "Reservera lokal POST /api/v1/bookings" fungerar inte som tänkt

---
+++ src/main/java/com/leander/cinema/exception/InvalidBookingException.java
@@ -0,0 +1,7 @@
+package com.leander.cinema.exception;
+
+public class InvalidBookingException extends RuntimeException {
+    public InvalidBookingException(String message) {
+        super(message);
+    }
+}
